3603e3188914c11927717f15abb198deaa18feb8
Doerminator <151198722+Doerminator@users.noreply.github.com>
2024-02-28 11:42:42 +0100

    edited Tests according to changes in Health class

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -6,33 +6,36 @@ public class UnitTest1
     [Fact]
     public void Health_Initialization_Test()
     {
-        int initialResistancePhysical = 10;
-        int initialResistanceHeat = 20;
-        int initialResistanceElectric = 30;
+        int initialResistancePhysical = 100;
+        int initialResistanceElectric = 300;
+        int initialVulnPhysical = 0;
+        int initialVulnElectric = 200;
 
-        Health health = new Health(100, 100, initialResistancePhysical, initialResistanceHeat, initialResistanceElectric);
+        Health health = new Health(1000, 1000, initialResistancePhysical, initialResistanceElectric, 
+            initialVulnPhysical, initialVulnElectric);
 
-        Assert.Equal(100, health._hpMax);
-        Assert.Equal(100, health._hp);
-        Assert.Equal(100, health._shield);
+        Assert.Equal(1000, health._hpMax);
+        Assert.Equal(1000, health._hp);
+        Assert.Equal(1000, health._shield);
         Assert.Equal(initialResistancePhysical, health._resistancePhysical);
-        Assert.Equal(initialResistanceHeat, health._resistanceHeat);
         Assert.Equal(initialResistanceElectric, health._resistanceElectric);
+        Assert.Equal(initialVulnPhysical, health._vulnPhysical);
+        Assert.Equal(initialVulnElectric, health._vulnElectric);
         Assert.True(health._alive);
     }
 
     [Theory]
-    [InlineData(1000, 1000, 200, 1000, 800, true)]
-    [InlineData(1000, 1000, 1000, 1000, 0, true)]
-    [InlineData(1000, 1000, 1200, 800, 0, true)]
-    [InlineData(1000, 1000, 1999, 1, 0, true)]
-    [InlineData(1000, 1000, 2000, 0, 0, false)]
+    [InlineData(1000, 1000, 200, 1000, 800, true)] //Damage only to Shield
+    [InlineData(1000, 1000, 1000, 1000, 0, true)] //Completely destroy shield
+    [InlineData(1000, 1000, 1200, 800, 0, true)] //Destroy shield with extra damage to health
+    [InlineData(1000, 1000, 3000, 0, 0, false)] //Overkill
+    [InlineData(1000, 1000, 2000, 0, 0, false)] //Exact deadly damage
     public void True_Damage_Test(
         int startHP, int startShield, int appliedDamage,
         int expectedHp, int expectedShield, bool expectedAlive)
     {
         // Arrange
-        Health health = new(startHP, startShield, 1000, 1000, 1000);
+        Health health = new(startHP, startShield, 1000, 1000, 0, 0);
 
         // Act
         health.ApplyTrueDamage(appliedDamage);
@@ -44,41 +47,26 @@ public class UnitTest1
     }
 
     [Theory]
-    [InlineData(0, 0, 0, 9000, 9000, 9000, 0, 0, false)]
-    public void Alive_Test(
-        int initialResistancePhysical, int initialResistanceHeat, int initialResistanceElectric,
-        int damagePhysical, int damageElectric, int damageHeat,
-        int expectedHp, int expectedShield, bool expectedAlive)
-    {
-        // Arrange
-        Health health = new Health(1000, 1000, initialResistancePhysical, initialResistanceHeat, initialResistanceElectric);
+    [InlineData(100, 100, 0, 0, 9000, 9000, 0, 0, false)] //Overkill
+    [InlineData(100, 100, 100, 100, 300, 300, 1000, 540, true)] //Calculation Test + no shield break
 
-        // Act
-        health.ApplyDamage(damagePhysical, damageElectric, damageHeat);
-
-        // Assert
-        Assert.Equal(expectedHp, health._hp);
-        Assert.Equal(expectedShield, health._shield);
-        Assert.Equal(expectedAlive, health._alive);
-    }
-
-
-    [Theory]
-    [InlineData(0, 0, 0, 200, 200, 200, 400, 1000, true)]
     public void ApplyDamage_Test(
-        int initialResistancePhysical, int initialResistanceHeat, int initialResistanceElectric,
-        int damagePhysical, int damageElectric, int damageHeat,
+        int initialResistancePhysical, int initialResistanceElectric,
+        int initialVulnPhysical, int initialVulnElectric,
+        int damagePhysical, int damageElectric, 
         int expectedHp, int expectedShield, bool expectedAlive)
     {
         // Arrange
-        Health health = new Health(1000, 1000, initialResistancePhysical, initialResistanceHeat, initialResistanceElectric);
+        Health health = new Health(1000, 1000, initialResistancePhysical, initialResistanceElectric,
+            initialVulnPhysical, initialVulnElectric);
 
         // Act
-        health.ApplyDamage(damagePhysical, damageElectric, damageHeat);
+        health.ApplyDamage(damagePhysical, damageElectric);
 
         // Assert
         Assert.Equal(expectedHp, health._hp);
         Assert.Equal(expectedShield, health._shield);
         Assert.Equal(expectedAlive, health._alive);
     }
+
 }
